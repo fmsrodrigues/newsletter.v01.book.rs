228090ee3a396f4ed144f4e212277ac47eacb5fe
Phobia <filipe_msr@hotmail.com>
2023-06-12 20:31:31 -0300

    Chapter 3: Change folder structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("localhost:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address.");
     tokio::spawn(server);
 
     format!("http://localhost:{}", port)
