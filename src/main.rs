d0caf9527334cf0f097715d3765692afa6f32ec8
Phobia <filipe_msr@hotmail.com>
2023-06-13 22:00:39 -0300

    Chapter 3: Development Environmental Variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener =
-        TcpListener::bind("http://localhost:8000").expect("Failed to bind server to port 8000");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("localhost:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind server to port 8000");
     run(listener)?.await
 }
