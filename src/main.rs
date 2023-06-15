2659e68b09832860d8901433fd517b595f08f92e
Phobia <filipe_msr@hotmail.com>
2023-06-15 17:26:05 -0300

    Chapter 3: fix tests, avoid using development database on tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -8,11 +8,11 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
     let address = format!("localhost:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind server to port 8000");
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
