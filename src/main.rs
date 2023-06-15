202175ad1109f55bcb95a90a0316a4bd55360099
Phobia <filipe_msr@hotmail.com>
2023-06-14 21:15:01 -0300

    Chapter 3: Connect Database to application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -7,7 +8,11 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("localhost:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind server to port 8000");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
