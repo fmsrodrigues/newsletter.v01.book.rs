09ffdebb213b9b9c2881fc0bb2c36e6418619c4a
Phobia <filipe_msr@hotmail.com>
2023-07-11 19:15:34 -0300

    Chapter 10: Add an login form page

--- src/startup.rs
+++ src/startup.rs
@@ -6,7 +6,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{get_connection_pool, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -72,6 +72,7 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/login", web::get().to(login_form))
             .route("/", web::get().to(home))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
